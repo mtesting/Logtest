121cfe472d7ea272d17b5cbdd54c2bd58a9814d6
mtesting <mtesting66@gmail.com>
2017-04-06 11:06:43 +0100

    testing

--- Test11/src/pack11/My1.java
+++ Test11/src/pack11/My1.java
@@ -1,12 +1,14 @@
 package pack11;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class My1 {
 
  WebDriver driver = new FirefoxDriver();
+	WebElement img1;
 	
 	
 }
